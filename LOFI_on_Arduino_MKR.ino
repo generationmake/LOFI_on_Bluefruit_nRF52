ba158eef4ed3fa977368abb939d6392758f527ab
Bernhard Mayer <bernhard@generationmake.de>
2020-01-12 09:52:47 +0100

    added send functions, decoded all values

--- LOFI_on_Arduino_MKR.ino
+++ LOFI_on_Arduino_MKR.ino
@@ -2,7 +2,8 @@
   This software emulates a HM-10 bluetooth module and the protocol used by the LOFI robot app, so that
   this app can control a robot using this Arduino MKR
 
-  By now there are only the motor control functions implemented. No analog in, no servo
+  By now there are only the motor control functions implemented and analog values are sent to app. 
+  The rest is decoded but no action connected.
   
   bernhard@generationmake.de
   Original Creation Date: Jan. 11, 2020
@@ -29,17 +30,15 @@ Ifx007t mot2;
 #define MOT2_2 5
 #define MOT2_EN 0
 
-const long interval = 5000;
+const long interval = 100;
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
 
-//int str[20];
-int str;
 BLEService lofiService("0000ffe0-0000-1000-8000-00805f9b34fb"); // BLE LOFI Service UUID of HM-10
 
 // BLE Serial Characteristic - custom 128-bit UUID of HM-10, read and writable by central
-BLEStringCharacteristic serialCharacteristic("0000ffe1-0000-1000-8000-00805f9b34fb", BLERead | BLEWriteWithoutResponse | BLENotify, 20);
+BLECharacteristic serialCharacteristic("0000ffe1-0000-1000-8000-00805f9b34fb", BLERead | BLEWriteWithoutResponse | BLENotify, 20);
 
 const int ledPin = LED_BUILTIN; // pin to use for the LED
 
@@ -80,9 +79,9 @@ void setup() {
 }
 
 void loop() {
-  String received;
+  uint8_t received[20];
   char prev_byte=0,current_byte=0;
-  int pwm_value;
+  int receive_length=0;
   // listen for BLE peripherals to connect:
   BLEDevice central = BLE.central();
 
@@ -94,44 +93,32 @@ void loop() {
 
     // while the central is still connected to peripheral:
     while (central.connected()) {
-/*
-// transfer data regularly to the app. not used yet
+
+// transfer data regularly to the app
       currentMillis = millis();
 
       if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;
-  //      sending();
-      } */
+        sending();
+      } 
+// receive data
       if (serialCharacteristic.written()) {   // check if data was sent by app
-        received=serialCharacteristic.value();  // get received string
-        Serial.print(received.c_str());     // print string to serial
+        receive_length=serialCharacteristic.readValue(received,20);  // get received string
+        Serial.print((char*)received);     // print string to serial
         Serial.print("  ");
-        for(int i=0; i<received.length(); i++)  // go through strin byte by byte
+        for(int i=0; i<receive_length; i++)  // go through string byte by byte
         {
           Serial.print(i);    // print byte and value of byte to serial
           Serial.print(":");
-          Serial.print(received.charAt(i));
+          Serial.print(received[i]);
           Serial.print("=");
-          Serial.print(byte(received.charAt(i)));
+          Serial.print(byte(received[i]));
           Serial.print("  ");
 
 //decode
-          current_byte=received.charAt(i);
-          if (prev_byte == 202) { // Motor A
-            if (current_byte <= 100) pwm_value=(current_byte * 2.55); //calculate pwm value
-            else pwm_value=(-((current_byte-100) * 2.55));
-            mot1.pwm(pwm_value);    // set pwm value
-            Serial.print(pwm_value);  // print pwm value to serial
-            Serial.print("!");
-          }
-          if (prev_byte == 203) { // Motor B
-            if (current_byte <= 100) pwm_value=(current_byte * 2.55);  //calculate pwm value
-            else pwm_value=(-((current_byte-100) * 2.55));
-            mot2.pwm(pwm_value);    // set pwm value
-            Serial.print(pwm_value);  // print pwm value to serial
-            Serial.print("!");
-          }
-          prev_byte=received.charAt(i);
+          current_byte=received[i];
+          decode_received(prev_byte,current_byte);
+          prev_byte=received[i];
         }
         Serial.println("");
       }
@@ -145,8 +132,61 @@ void loop() {
   }
 }
 
+//decode received bytes and set outputs
+void decode_received(byte prev_byte, byte current_byte)
+{
+  int pwm_value;
+
+  if (prev_byte == 201) { // Buzzer
+    digitalWrite(ledPin, current_byte);
+  }
+  if (prev_byte == 202) { // Motor 1
+    if (current_byte <= 100) pwm_value=(current_byte * 2.55); //calculate pwm value
+    else pwm_value=(-((current_byte-100) * 2.55));
+    mot1.pwm(pwm_value);    // set pwm value
+    Serial.print(pwm_value);  // print pwm value to serial
+    Serial.print("!");
+  }
+  if (prev_byte == 203) { // Motor 2
+    if (current_byte <= 100) pwm_value=(current_byte * 2.55);  //calculate pwm value
+    else pwm_value=(-((current_byte-100) * 2.55));
+    mot2.pwm(pwm_value);    // set pwm value
+    Serial.print(pwm_value);  // print pwm value to serial
+    Serial.print("!");
+  }
+  if (prev_byte == 204) { // output 1
+  
+  }
+  if (prev_byte == 205) { // output 2
+  
+  }
+  if (prev_byte == 206) { // output 3
+  
+  }
+  if (prev_byte == 207) { // output 4
+  
+  }
+  if (prev_byte == 208) { // servo output 1
+  
+  }
+  if (prev_byte == 209) { // servo output 2
+  
+  }
+  if (prev_byte == 210) { // servo output 3
+  
+  }
+  if (prev_byte == 211) { // servo output 4
+  
+  }
+  if (prev_byte == 212 && current_byte == 99) {
+  
+  }
+  
+}
+
 // function for sending values to the app. just copied from LOFI sample, not modified yet
 void sending() {
+  byte sendstring[20];
   int analog1 = 0;
   int analog2 = 0;
   int analog3 = 0;
@@ -157,21 +197,18 @@ void sending() {
   analog4 = analogRead(3)/10.23;
   
 //[224, 115, 2, 225, 102, 4, 226, 107, 5, 227, 63, 6]
-  serialCharacteristic.writeValue("test");
-/*  serialCharacteristic.writeValue(224);
-  serialCharacteristic.writeValue(byte(analog1));
-  serialCharacteristic.writeValue(225);
-  serialCharacteristic.writeValue(byte(analog2));
-  serialCharacteristic.writeValue(226);
-  serialCharacteristic.writeValue(byte(analog3));
-  serialCharacteristic.writeValue(227);
-  serialCharacteristic.writeValue(byte(analog4));
-
-  //odleglosc();
-
-  serialCharacteristic.writeValue(240);
-  serialCharacteristic.writeValue(100);
+  sendstring[0]=224;
+  sendstring[1]=byte(analog1);
+  sendstring[2]=225;
+  sendstring[3]=byte(analog2);
+  sendstring[4]=226;
+  sendstring[5]=byte(analog3);
+  sendstring[6]=227;
+  sendstring[7]=byte(analog4);
+  sendstring[8]=240;
+  sendstring[9]=100;
   // last byte "i" character as a delimiter for BT2.0 on Android
-  serialCharacteristic.writeValue(105);
- */
+  sendstring[10]=105;
+
+  serialCharacteristic.writeValue((const uint8_t*)sendstring,11);
 }
